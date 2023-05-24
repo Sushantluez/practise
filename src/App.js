e59cb401b86b37980bc7d379e6dd6737b30e13ef
Sushantluez <sushantlama49@gmail.com>
2023-05-24 21:22:41 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './pages/components/Header';
-import { person, person1 } from './pages/components/data';
+import { person, person1 } from './pages/components/Data';
 import { Route, Routes } from 'react-router';
 import RootLayOut from './pages/RootLayOut';
 import HomePages from './pages/components/HomePages';
 import About from './pages/About';
 import Pagenotfound from './pages/Pagenotfound';
+import Java from './pages/Java';
 // import ContactPages from './pages/contactPages';
 // import AboutDetails from './pages/components/AboutDetails';
 
@@ -37,9 +38,15 @@ const App = () => {
           <Route index element={<HomePages />} />
 
           <Route path='about' element={<About />} />
+
+
+
           {/* <Route path='aboutDetails' element={<AboutDetails />} /> */}
           {/* </Route>
           <Route path='contact' element={<ContactPages />} /> */}
+
+
+
 
           <Route path='*' element={<Pagenotfound />} />
 
@@ -52,6 +59,9 @@ const App = () => {
 
 
       </Routes>
+
+
+      {/* <Java /> */}
 
 
 
--- src/pages/java.js
+++ src/pages/java.js
@@ -1,473 +1,484 @@
-// const printAge = () => {
-//   console.log(age)
-// }
+import React from 'react'
 
+const Java = () => {
+  return (
+    // const printAge = () => {
+    //   console.log(age)
+    // }
 
-//string number booleam undefined null
-// const personName = 'ramhari is bad';
-// const firstname = 'ram';
-// const lastname = 'sharma';
-// const amount = 90;
-// const detail = 'millions';
-// console.log(detail.substring(0, 5) + '...');
-// console.log(personName.slice(3, 7));
 
+    //string number booleam undefined null
+    // const personName = 'ramhari is bad';
+    // const firstname = 'ram';
+    // const lastname = 'sharma';
+    // const amount = 90;
+    // const detail = 'millions';
+    // console.log(detail.substring(0, 5) + '...');
+    // console.log(personName.slice(3, 7));
 
-// const per = personName.replace('bad', 'good')
-// console.log(per);
-// const templatestring = `${personName} ${firstname} ${lastname} ${amount}`;
 
-// console.log(amount.toString())
+    // const per = personName.replace('bad', 'good')
+    // console.log(per);
+    // const templatestring = `${personName} ${firstname} ${lastname} ${amount}`;
 
-// const m = personName.toLocaleUpperCase();
+    // console.log(amount.toString())
 
-// console.log(m);
+    // const m = personName.toLocaleUpperCase();
 
-// console.log(personName.includes('ari'));
-// console.log(personName.charAt(3));
-// console.log(personName.trim().length);
-// console.log(personName.indexOf('r'));
-// console.log(personName.repeat(5));
-// console.log(typeof (amount));
-// console.log(firstname.concat('', lastname, ' he is '));
-// console.log('hello' + ' ' + 'world');
+    // console.log(m);
 
+    // console.log(personName.includes('ari'));
+    // console.log(personName.charAt(3));
+    // console.log(personName.trim().length);
+    // console.log(personName.indexOf('r'));
+    // console.log(personName.repeat(5));
+    // console.log(typeof (amount));
+    // console.log(firstname.concat('', lastname, ' he is '));
+    // console.log('hello' + ' ' + 'world');
 
 
-// const amount = '900';
-// const g = Infinity;
-// const rating = '90.90';
-// const m = Number.parseInt(amount);
-// const r = Number.parseFloat(rating);
-// console.log(m);
-// console.log(Number.isFinite(g));
 
+    // const amount = '900';
+    // const g = Infinity;
+    // const rating = '90.90';
+    // const m = Number.parseInt(amount);
+    // const r = Number.parseFloat(rating);
+    // console.log(m);
+    // console.log(Number.isFinite(g));
 
 
 
-// 2023/5/5
 
+    // 2023/5/5
 
-// const rating = 90.4548732;
-// const m = 4;
-// const r = rating.toFixed(2);
-// console.log(r);
 
-// console.log(Math.sqrt(4));
-// console.log(Math.ceil(rating));
-// console.log(Math.round(rating));
-// console.log(Math.floor(rating));
-// console.log(Math.max(22, 77, 55, 44, 99));
-// console.log(Math.min(22, 77, 55, 44, 99));
-// console.log(Math.pow(3, 3));
+    // const rating = 90.4548732;
+    // const m = 4;
+    // const r = rating.toFixed(2);
+    // console.log(r);
 
-// const random = Math.random() * 10 + 1;
-// console.log(Math.floor(random));
+    // console.log(Math.sqrt(4));
+    // console.log(Math.ceil(rating));
+    // console.log(Math.round(rating));
+    // console.log(Math.floor(rating));
+    // console.log(Math.max(22, 77, 55, 44, 99));
+    // console.log(Math.min(22, 77, 55, 44, 99));
+    // console.log(Math.pow(3, 3));
 
-// console.log(rating.toFixed(2))
+    // const random = Math.random() * 10 + 1;
+    // console.log(Math.floor(random));
 
+    // console.log(rating.toFixed(2))
 
 
-// const a = 20;
 
-// let c = 1;
+    // const a = 20;
 
-// c = c + 5;
-// c += 5;
-// c -= 5;
-// c *= 5;
-// c /= 5;
+    // let c = 1;
 
-// console.log(c);
+    // c = c + 5;
+    // c += 5;
+    // c -= 5;
+    // c *= 5;
+    // c /= 5;
 
-// const d = ++c;
+    // console.log(c);
 
-// console.log(d)
+    // const d = ++c;
 
+    // console.log(d)
 
-// let mi = 900;
-// ++mi;
-// console.log(mi);
 
-// const b = 90;
-// const simplify = 90 + (90 * 90) / 9;
-// console.log(simplify)
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(b % a);
+    // let mi = 900;
+    // ++mi;
+    // console.log(mi);
 
+    // const b = 90;
+    // const simplify = 90 + (90 * 90) / 9;
+    // console.log(simplify)
+    // console.log(a + b);
+    // console.log(a - b);
+    // console.log(a * b);
+    // console.log(a / b);
+    // console.log(b % a);
 
-// greater than less than
 
+    // greater than less than
 
-// const a = 90;
-// const b = 100;
 
+    // const a = 90;
+    // const b = 100;
 
-// console.log(a > b);
-// console.log(a < b);
-// console.log(a >= b);
-// console.log(a <= b);
-// console.log(a === b);
-// console.log(a !== b);
 
+    // console.log(a > b);
+    // console.log(a < b);
+    // console.log(a >= b);
+    // console.log(a <= b);
+    // console.log(a === b);
+    // console.log(a !== b);
 
-// console.log(a > b || a < b || a === b);
-// console.log(a > b && a < b && a !== b);
 
-// const msg = a > b ? 'ok' : a === b ? 'lio' : 90909;
+    // console.log(a > b || a < b || a === b);
+    // console.log(a > b && a < b && a !== b);
 
-// 'wrong'
+    // const msg = a > b ? 'ok' : a === b ? 'lio' : 90909;
 
-// if (a > b) {
-//   return <h1> This is the truly condition</h1>
-// } else {
-//   return <h1> Something to be  executes</h1>
+    // 'wrong'
 
-// }
+    // if (a > b) {
+    //   return <h1> This is the truly condition</h1>
+    // } else {
+    //   return <h1> Something to be  executes</h1>
 
+    // }
 
 
 
-// const age = 30;
 
+    // const age = 30;
 
-// if (age < 17) {
-//   return <h1>You are junior, price is Rs.20</h1>
-// } else if (age > 17 && age < 50) {
-//   return <h1>You are regular, price is Rs.50</h1>
-// } else {
-//   return <h1>You are senior, price is Rs.20</h1>
-// }
 
+    // if (age < 17) {
+    //   return <h1>You are junior, price is Rs.20</h1>
+    // } else if (age > 17 && age < 50) {
+    //   return <h1>You are regular, price is Rs.50</h1>
+    // } else {
+    //   return <h1>You are senior, price is Rs.20</h1>
+    // }
 
-// task
 
+    // task
 
-// const salary = 50000;
-// const expenses = 40000;
 
-// if (salary > expenses) {
-//   return <h1>you have saved {` ${salary - expenses} `} this month</h1>
-// }
-// else if (expenses > salary) {
-//   return <h1>you have lost{` ${expenses - salary}`} this month</h1>
-// }
-// else
-//   return <h1>Your balance hasnot changed</h1>
+    // const salary = 50000;
+    // const expenses = 40000;
 
-// let i = 1;
-// let text = '';
-// while (i < 20) {
-//   i++;
-// console.log(i);
-//   text = text + `${i}`;
-// }
+    // if (salary > expenses) {
+    //   return <h1>you have saved {` ${salary - expenses} `} this month</h1>
+    // }
+    // else if (expenses > salary) {
+    //   return <h1>you have lost{` ${expenses - salary}`} this month</h1>
+    // }
+    // else
+    //   return <h1>Your balance hasnot changed</h1>
 
-// do {
-//   i++;
-//   text = text + `${i}`;
-// } while (i < 20)
+    // let i = 1;
+    // let text = '';
+    // while (i < 20) {
+    //   i++;
+    // console.log(i);
+    //   text = text + `${i}`;
+    // }
 
+    // do {
+    //   i++;
+    //   text = text + `${i}`;
+    // } while (i < 20)
 
-// for (let i = 1; i <= 15; i++) {
 
-// if (i === 10) {
-//   break;
-//   continue;
+    // for (let i = 1; i <= 15; i++) {
 
-// if (i % 3 === 0 && i % 5 === 0) {
-//   console.log(`fizz buzz ${i}`);
-// } else if (i % 3 === 0) {
-//   console.log(`fizz ${i}`);
-// } else if (i % 5 === 0) {
-//   console.log(`buzz ${i}`)
-// } else {
-//   console.log(i);
-// }
+    // if (i === 10) {
+    //   break;
+    //   continue;
 
-// }
+    // if (i % 3 === 0 && i % 5 === 0) {
+    //   console.log(`fizz buzz ${i}`);
+    // } else if (i % 3 === 0) {
+    //   console.log(`fizz ${i}`);
+    // } else if (i % 5 === 0) {
+    //   console.log(`buzz ${i}`)
+    // } else {
+    //   console.log(i);
+    // }
 
-// const weekDay = 3;
+    // }
 
-// switch (weekDay) {
-//   case 1:
-//     console.log('sunday');
-//     break;
-//   case 2:
-//     console.log('monday');
-//     break;
-//   default:
-//     console.log('not a weekDay')
-// }
+    // const weekDay = 3;
 
-// let i;
+    // switch (weekDay) {
+    //   case 1:
+    //     console.log('sunday');
+    //     break;
+    //   case 2:
+    //     console.log('monday');
+    //     break;
+    //   default:
+    //     console.log('not a weekDay')
+    // }
 
-// const data = null;
-// if (data === null) {
-//   console.log('data not available');
-// }
+    // let i;
 
-// if (i === undefined) {
-//   i = 90;
-// }
+    // const data = null;
+    // if (data === null) {
+    //   console.log('data not available');
+    // }
 
-// const numbers = [22, 55, 66, 77, 88, 99];
-// for (let a in numbers) {
-//   console.log(a);
-// }
+    // if (i === undefined) {
+    //   i = 90;
+    // }
 
-// let total = 1;
-// for (let a of numbers) {
-//   total = total + a;
-// }
+    // const numbers = [22, 55, 66, 77, 88, 99];
+    // for (let a in numbers) {
+    //   console.log(a);
+    // }
 
+    // let total = 1;
+    // for (let a of numbers) {
+    //   total = total + a;
+    // }
 
-// function greet() {
-//   console.log('hello user');
-// }
 
-// greet();
+    // function greet() {
+    //   console.log('hello user');
+    // }
 
-// const greet = (greet, user) => {
-//   alert(`${greet} ${user}`)
-// }
+    // greet();
 
-// window.addEventListener('resize', () => {
-//   console.log('hello');
-// });
+    // const greet = (greet, user) => {
+    //   alert(`${greet} ${user}`)
+    // }
 
-// const age = prompt('what is  your age');
+    // window.addEventListener('resize', () => {
+    //   console.log('hello');
+    // });
 
-// const hio = window.confirm('are you sure');
+    // const age = prompt('what is  your age');
 
-// console.log(hio);
+    // const hio = window.confirm('are you sure');
 
-// if (age < 20) {
-//   return <h1>your are Junior</h1>
-// } else if (age > 20 && age > 50) {
-//   return <h1>your are senior</h1>
-// }
+    // console.log(hio);
 
+    // if (age < 20) {
+    //   return <h1>your are Junior</h1>
+    // } else if (age > 20 && age > 50) {
+    //   return <h1>your are senior</h1>
+    // }
 
-// let total = 0;
 
-// const sumOfNumbers = (numbers) => {
+    // let total = 0;
 
-//   for (let i of numbers) {
-//     total = total + i;
-//   }
-//   return total;
-// }
+    // const sumOfNumbers = (numbers) => {
 
-// const m = sumOfNumbers([22, 77, 44, 99]);
+    //   for (let i of numbers) {
+    //     total = total + i;
+    //   }
+    //   return total;
+    // }
 
+    // const m = sumOfNumbers([22, 77, 44, 99]);
 
 
 
-// arraycourse
 
-// const numbers = [22, 55, 77, 99];
-// const persons = ['ram', 'hari', 'shyam', 'sita'];
+    // arraycourse
 
-// 5/12
+    // const numbers = [22, 55, 77, 99];
+    // const persons = ['ram', 'hari', 'shyam', 'sita'];
 
-// const numbers = [1, 2, 20, 4, 5, 6, 0];
+    // 5/12
 
-// const n = numbers.reduce
-// ((p, n) => {
-//   return p + n;
-// })
-// ((p, n) => {
-//   return p > n ? n : p;
-// })
+    // const numbers = [1, 2, 20, 4, 5, 6, 0];
 
-// const n = numbers.find((n) => {
-//   return n > 6;
-// });
+    // const n = numbers.reduce
+    // ((p, n) => {
+    //   return p + n;
+    // })
+    // ((p, n) => {
+    //   return p > n ? n : p;
+    // })
 
-// const d = numbers.filter((n) => {
-//   return n > 2;
-// })
+    // const n = numbers.find((n) => {
+    //   return n > 6;
+    // });
 
-// console.log(n);
-// console.log(d);
+    // const d = numbers.filter((n) => {
+    //   return n > 2;
+    // })
 
+    // console.log(n);
+    // console.log(d);
 
-// const data = [
-//   { id: 1, name: 'ram', gender: 'male' },
-//   { id: 2, name: 'rita', gender: 'female' },
-//   { id: 3, name: 'sita', gender: 'female' },
-// ];
 
-// const femaleData = data.filter((d) => {
-//   return d.gender === 'female';
-// });
+    // const data = [
+    //   { id: 1, name: 'ram', gender: 'male' },
+    //   { id: 2, name: 'rita', gender: 'female' },
+    //   { id: 3, name: 'sita', gender: 'female' },
+    // ];
 
-// const singleData = data.find((d) => {
-//   return d.name === 'ram';
-// });
+    // const femaleData = data.filter((d) => {
+    //   return d.gender === 'female';
+    // });
 
-// console.log(femaleData);
-// console.log(singleData);
+    // const singleData = data.find((d) => {
+    //   return d.name === 'ram';
+    // });
 
+    // console.log(femaleData);
+    // console.log(singleData);
 
 
 
-// let total = '';
-// numbers.forEach((n, i) => {
-//   total = total + n;
-// });
 
-// let names = '';
+    // let total = '';
+    // numbers.forEach((n, i) => {
+    //   total = total + n;
+    // });
 
-// persons.forEach((m) => {
+    // let names = '';
 
-//   names = names + m[0];
-//   // console.log(m);
-// })
+    // persons.forEach((m) => {
 
+    //   names = names + m[0];
+    //   // console.log(m);
+    // })
 
-// const newPersons = persons.map((p) => {
-//   return p + 'per';
-// });
-// console.log(newPersons);
 
-// const newNumbers = numbers.map((i) => {
-//   return i * 5;
-// });
+    // const newPersons = persons.map((p) => {
+    //   return p + 'per';
+    // });
+    // console.log(newPersons);
 
-// console.log(newNumbers);
+    // const newNumbers = numbers.map((i) => {
+    //   return i * 5;
+    // });
 
-// persons.reverse();
-// persons.sort();
+    // console.log(newNumbers);
 
+    // persons.reverse();
+    // persons.sort();
 
-// console.log(persons.includes('rita'))
-// console.log(persons.join(' and '));
-// 
-// console.log(numbers.join()[4])
 
+    // console.log(persons.includes('rita'))
+    // console.log(persons.join(' and '));
+    //
+    // console.log(numbers.join()[4])
 
 
-// numbers.push(101);
-// numbers.unshift(11)
-// numbers.shift();
-// numbers.pop();
-// const m = numbers.slice(2, 5);
-// console.log(m)
-// numbers.splice(2, 3);
-// console.log(numbers);
 
-// console.log(numbers.at(2));
-// console.log(numbers.concat(100, 999))
+    // numbers.push(101);
+    // numbers.unshift(11)
+    // numbers.shift();
+    // numbers.pop();
+    // const m = numbers.slice(2, 5);
+    // console.log(m)
+    // numbers.splice(2, 3);
+    // console.log(numbers);
 
+    // console.log(numbers.at(2));
+    // console.log(numbers.concat(100, 999))
 
-// 5/14 destructing
 
-// const numbers = [22, 55, 77, 99]
+    // 5/14 destructing
 
-// const [m, b, a, n, u] = numbers;
+    // const numbers = [22, 55, 77, 99]
 
+    // const [m, b, a, n, u] = numbers;
 
-// const person = {
-//   name: 'hariram',
-//   age: 90,
-//   habits: ['dance', 'sing'],
-// isAdmin: false,
-// sleep: function () {
-//   return this.sleep;
-// }
 
+    // const person = {
+    //   name: 'hariram',
+    //   age: 90,
+    //   habits: ['dance', 'sing'],
+    // isAdmin: false,
+    // sleep: function () {
+    //   return this.sleep;
+    // }
 
-// const persons = ['ram', 'shyam'];
 
-// const [x, y] = persons;
+    // const persons = ['ram', 'shyam'];
 
-// const car = {
-//   ids: 1,
-//   cartType: 'BMW',
-//   price: 900000
-// };
+    // const [x, y] = persons;
 
+    // const car = {
+    //   ids: 1,
+    //   cartType: 'BMW',
+    //   price: 900000
+    // };
 
-// const { ids, cartType } = car;
 
-// const dat = [
-//   { n: 50, rates: [7.5, 9.9] },
-//   { n: 70, rates: [6.5, 7.7] },
-// ];
+    // const { ids, cartType } = car;
 
-// const [{ n, rates: [v] }, k] = dat;
+    // const dat = [
+    //   { n: 50, rates: [7.5, 9.9] },
+    //   { n: 70, rates: [6.5, 7.7] },
+    // ];
 
+    // const [{ n, rates: [v] }, k] = dat;
 
-// const data = [
-//   {
-//     name: 'hariram',
-//     age: 90,
-//     habits: ['dance', 'sing'],
-//     datas: [
-//       { id: 1, mi: [1, 4, 5] }
-//     ]
-//   },
-//   {
-//     name: 'hariram2',
-//     age: 90,
-//     habits: ['dance', 'sing'],
-//   }
-// ];
 
-// const p = {
-//   id: 1,
-//   title: 'hello ram'
-// };
-// const { id, title } = p;
+    // const data = [
+    //   {
+    //     name: 'hariram',
+    //     age: 90,
+    //     habits: ['dance', 'sing'],
+    //     datas: [
+    //       { id: 1, mi: [1, 4, 5] }
+    //     ]
+    //   },
+    //   {
+    //     name: 'hariram2',
+    //     age: 90,
+    //     habits: ['dance', 'sing'],
+    //   }
+    // ];
 
-// const [{ name, habits: [a, s], datas: [{ mi: [e] }] }, b] = data;
+    // const p = {
+    //   id: 1,
+    //   title: 'hello ram'
+    // };
+    // const { id, title } = p;
 
-// console.log(e);
+    // const [{ name, habits: [a, s], datas: [{ mi: [e] }] }, b] = data;
 
+    // console.log(e);
 
 
 
-// datetime
 
-// const n = new Date();
-// console.log(n.getHours());
+    // datetime
 
-// const d = new Date("1995-11-26")
-// console.log(d);
+    //   const n = new Date();
+    // console.log(n.getHours());
 
+    // const d = new Date("1995-11-26")
+    // console.log(d);
 
-// const greet = () => {
-//   console.log('hello user');
-// }
 
-// setTimeout(() => {
-//   greet();
-// }, 5000)
+    // const greet = () => {
+    //   console.log('hello user');
+    // }
 
+    // setTimeout(() => {
+    //   greet();
+    // }, 5000)
 
-// setInterval(() => {
-//   const n = new Date();
-//   console.log(`${n.getHours()}/${n.getMinutes()}/${n.getSeconds()}`);
-// },)
 
+    // setInterval(() => {
+    //   const n = new Date();
+    //   console.log(`${n.getHours()}/${n.getMinutes()}/${n.getSeconds()}`);
+    // },)
 
 
-// const m = setInterval(() => {
-//   const n = Date();
-//   console.log(n);
-// }, 1000);
 
+    // const m = setInterval(() => {
+    //   const n = Date();
+    //   console.log(n);
+    // }, 1000);
 
-// setTimeout(() => {
-//   clearInterval(m);
-// }, 5000);
 
+    // setTimeout(() => {
+    //   clearInterval(m);
+    // }, 5000);
 
-// const mino = 'HEllo';
 
-export const App
+    // const mino = 'HEllo';
+
+
+    <div>
+
+    </div>
+  )
+}
+
+export default Java
